7effa13745517e087548bfe40a022c4a3cf44306
Daniel Kostro <kostro.d@gmail.com>
2018-06-01 17:51:03 +0200

    implementation and test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,87 @@
-import myModule from '..';
+import paramGrid from '..';
 
 describe('test myModule', () => {
-    it('should return 42', () => {
-        expect(myModule()).toEqual(42);
-    });
+  it('should return 42', () => {
+    expect([
+      ...paramGrid({
+        a: [1, 2],
+        b: ['a', 'b', 'c'],
+        c: [{ x: 1 }, { x: 2 }],
+        d: true
+      })
+    ]).toEqual([
+      {
+        a: 1,
+        b: 'a',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 1,
+        b: 'a',
+        c: { x: 2 },
+        d: true
+      },
+      {
+        a: 1,
+        b: 'b',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 1,
+        b: 'b',
+        c: { x: 2 },
+        d: true
+      },
+      {
+        a: 1,
+        b: 'c',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 1,
+        b: 'c',
+        c: { x: 2 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'a',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'a',
+        c: { x: 2 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'b',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'b',
+        c: { x: 2 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'c',
+        c: { x: 1 },
+        d: true
+      },
+      {
+        a: 2,
+        b: 'c',
+        c: { x: 2 },
+        d: true
+      }
+    ]);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 /**
+ * @generator
+ * @param {object} options - An object. Each key should countain an array of possible values for this key. If not an array, then the given value is the only possible value for this parameter.
  * Returns a very important number
- * @return {number}
+ * @yields {object} - The generated parameter object.
  */
-export default function myModule() {
-    return 42;
+export default function* paramGrid(options) {
+  options = Object.assign({}, options);
+  const keys = Object.keys(options);
+  let total = 1;
+  let lengths = [];
+  let cumLengths = [];
+
+  for (let i = keys.length - 1; i >= 0; i--) {
+    const key = keys[i];
+    if (!Array.isArray(options[key])) {
+      options[key] = [options[key]];
+    }
+    total *= options[key].length;
+    lengths[i] = options[key].length;
+    cumLengths[i] = total;
+  }
+  cumLengths.push(1);
+
+  for (let i = 0; i < total; i++) {
+    const params = {};
+    for (let j = keys.length - 1; j >= 0; j--) {
+      params[keys[j]] =
+        options[keys[j]][Math.floor(i / cumLengths[j + 1]) % lengths[j]];
+    }
+    yield params;
+  }
 }
